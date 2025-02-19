0680eb24963577c7cff3840455bc0122db01cf76
joaoluiz00 <joao.luiz.souza.pereira@gmail.com>
2025-02-19 17:25:08 -0300

    o inicio de tudo

---
+++ Banco/jdb.sql
@@ -0,0 +1,40 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 19-Fev-2025 às 21:23
+-- Versão do servidor: 10.4.27-MariaDB
+-- versão do PHP: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `jdb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `teste`
+--
+
+CREATE TABLE `teste` (
+  `cod` int(11) NOT NULL,
+  `nome` int(11) NOT NULL,
+  `naosei` int(11) NOT NULL,
+  `teste` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # JDB
 Projeto anual da Matéria de modelagem
+
+Faça comentarios ou anotações sobre o projeto abaixo:
+
+Exemplo de loja indicado pelo bruno - https://mypcards.com/ 
